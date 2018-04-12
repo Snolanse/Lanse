3db966526bff5d33185e917ca7cb656c05be2a31
hakfj <haakonflisnes@gmail.com>
2018-04-12 13:25:12 +0200

    pwm igjen

--- GUI/pwm.py
+++ GUI/pwm.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setwarnings(False)
 
@@ -11,4 +12,14 @@ GPIO.setup(PWM1, GPIO.OUT)
 
 p = GPIO.PWM(PWM0, 100)  # (channel, freq)
 p.start(0)  # duty cycle [0 - 100]
+
+while 1:                               #execute loop forever
+
+    for x in range (50):                          #execute loop for 50 times, x being incremented from 0 to 49.
+        p.ChangeDutyCycle(x)               #change duty cycle for varying the brightness of LED.
+        time.sleep(0.1)                           #sleep for 100m second
+
+    for x in range (50):                         #execute loop for 50 times, x being incremented from 0 to 49.
+        p.ChangeDutyCycle(50-x)        #change duty cycle for changing the brightness of LED.
+        time.sleep(0.1)                          #sleep for 100m second
 
