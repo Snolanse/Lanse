a4c5caf8d0844cb6c82666234cd2766ad5ec160b
Tjore <35220757+Tjore@users.noreply.github.com>
2018-04-16 13:19:23 +0200

    Kommunikasjon
    splittet serverCom i pakker for sending og henting

--- GUI/Modules/csrf.py
+++ GUI/Modules/csrf.py
@@ -2,6 +2,9 @@ import requests
 import json
 import time
 
+global go
+go = 1
+
 def serverCom(bronnid, get, sdata):
 
     url1 = "http://192.168.0.101:8000/csrf"
@@ -20,3 +23,17 @@ def serverCom(bronnid, get, sdata):
 
     r = client.post(url2, data=l_data, headers=dict(Referer=url2))
     return(json.loads(r.content.decode()))
+
+def serverSend(bronnid, sdata):
+    global go
+    go = 0
+    mdata = serverCom(bronnid,0,sdata)
+    return mdata
+
+def serverhent(bronnid):
+    global go
+    go = 1
+    mdata = serverCom(bronnid,1,{})
+    if go == 1:
+        global serverData
+        serverData = mdata
